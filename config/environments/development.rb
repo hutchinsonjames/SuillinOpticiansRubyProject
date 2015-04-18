69ee23499457b971dcb986e9cee2061428cae30a
hutchinsonjames <hutchinson.james+cloud9@gmail.com>
2015-04-18 21:49:05 +0000

    Email confirmation with SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,11 +37,10 @@ Rails.application.configure do
     password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'hutchinsonjames-richonrails-1434654' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
 
 
   # Adds additional error checking when serving assets at runtime.
